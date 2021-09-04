d3c86308d466b780bf688cb640da87ab56b01fee
BrianBleau <bleaubrian@gmail.com>
2021-09-04 10:16:45 -0400

    Added timer and high score

--- script.js
+++ script.js
@@ -2,17 +2,50 @@
 
 const cards = document.querySelectorAll(".card");
 const cardInner = document.querySelectorAll(".card-inner");
+const labelClock = document.querySelector(".clock-display");
+const highscoreDisplay = document.querySelector(".highscore-display");
+let highscore;
 let firstGuess;
 let secondGuess;
 let flippedCard = false;
 let lockedBoard = false;
+let clockRunning = false;
+let matched = 0;
 
 function found(first, second) {
   firstGuess.removeEventListener("click", flipCard);
   secondGuess.removeEventListener("click", flipCard);
 }
 
+const startTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0);
+    const sec = String(time % 60).padStart(2, 0);
+    if (matched === 9) {
+      clearInterval(timer);
+      if (!highscore) {
+        highscore = time;
+        let minHigh = String(Math.trunc(highscore / 60)).padStart(2, 0);
+        let secHigh = String(highscore % 60).padStart(2, 0);
+        highscoreDisplay.textContent = `${minHigh}:${secHigh}`;
+      }
+    }
+    labelClock.textContent = `${min}:${sec}`;
+    time++;
+  };
+
+  let time = 0;
+  tick();
+  const timer = setInterval(tick, 1000);
+
+  return timer;
+};
+
 const flipCard = function () {
+  if (clockRunning === false) {
+    startTimer();
+    clockRunning = true;
+  }
   //return if clicking on same card
   if (this === firstGuess) return;
   //return if pending timeout
@@ -25,6 +58,8 @@ const flipCard = function () {
     secondGuess = this;
     flippedCard = false;
     if (firstGuess.dataset.image === secondGuess.dataset.image) {
+      matched++;
+      console.log(matched);
       found(firstGuess, secondGuess);
     } else {
       lockedBoard = true;
