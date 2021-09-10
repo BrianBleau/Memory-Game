7f54a2a635b992088a07e3d4343d464787ba2b25
BrianBleau <bleaubrian@gmail.com>
2021-09-10 06:15:46 -0400

    Added Persistent Users/Scores

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const cards = document.querySelectorAll(".card");
 const cardInner = document.querySelectorAll(".card-inner");
 const labelClock = document.querySelector(".clock-display");
 const highscoreDisplay = document.querySelector(".highscore-display");
+const nameInput = document.querySelector(".name-input");
+const nameButton = document.querySelector(".name-button");
+const players = [];
+const nameDisplay = document.querySelector(".name-display");
+const playerHighDisplay = document.querySelector(".player-high");
+const popup = document.querySelector(".popup");
+const popupText = document.querySelector(".popup-text");
+let playing = {};
 let highscore;
 let firstGuess;
 let secondGuess;
@@ -12,25 +20,70 @@ let lockedBoard = false;
 let clockRunning = false;
 let matched = 0;
 
+const resetClock = () => (labelClock.textContent = "0:00");
+
+const calcPlayerHigh = function () {
+  let time = playing.playerHigh;
+  const min = String(Math.trunc(time / 60)).padStart(2, 0);
+  const sec = String(time % 60).padStart(2, 0);
+  playerHighDisplay.textContent = `${playing.playerName}'s High: ${min}:${sec}`;
+};
+
 function found(first, second) {
   firstGuess.removeEventListener("click", flipCard);
   secondGuess.removeEventListener("click", flipCard);
 }
 
+nameButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  matched = 0;
+  playing = {};
+  let current = players.find((o) => o.playerName === nameInput.value);
+  if (current) {
+    playing = current;
+    nameDisplay.textContent = `Playing: ${playing.playerName}`;
+    popup.classList.toggle("hidden");
+    calcPlayerHigh();
+    flipAll();
+    setTimeout(shuffle, 600);
+    cardInner.forEach((el) => el.addEventListener("click", flipCard));
+  } else {
+    let obj = {
+      playerName: nameInput.value,
+      hasHigh: false,
+    };
+    players.push(obj);
+    playing = obj;
+    nameDisplay.textContent = `Playing: ${playing.playerName}`;
+    popup.classList.toggle("hidden");
+    flipAll();
+    setTimeout(shuffle, 600);
+    cardInner.forEach((el) => el.addEventListener("click", flipCard));
+  }
+});
+
 const startTimer = function () {
   const tick = function () {
     const min = String(Math.trunc(time / 60)).padStart(2, 0);
     const sec = String(time % 60).padStart(2, 0);
+    labelClock.textContent = `${min}:${sec}`;
     if (matched === 9) {
       clearInterval(timer);
-      if (!highscore) {
+      if (!playing.playerHigh || time < playing.playerHigh) {
+        playing.playerHigh = time;
+      }
+
+      if (!highscore || time < highscore) {
         highscore = time;
         let minHigh = String(Math.trunc(highscore / 60)).padStart(2, 0);
         let secHigh = String(highscore % 60).padStart(2, 0);
-        highscoreDisplay.textContent = `${minHigh}:${secHigh}`;
+        highscoreDisplay.textContent = `Best Overall(${playing.playerName}):${minHigh}:${secHigh}`;
       }
+      clockRunning = false;
+      popup.classList.remove("hidden");
+      popupText.textContent = `Congrats, ${playing.playerName}, your time is ${playing.playerHigh} seconds. Who's next?`;
     }
-    labelClock.textContent = `${min}:${sec}`;
+
     time++;
   };
 
@@ -59,7 +112,6 @@ const flipCard = function () {
     flippedCard = false;
     if (firstGuess.dataset.image === secondGuess.dataset.image) {
       matched++;
-      console.log(matched);
       found(firstGuess, secondGuess);
     } else {
       lockedBoard = true;
@@ -78,6 +130,12 @@ const flip = cardInner.forEach((card) =>
   card.addEventListener("click", flipCard)
 );
 
+const flipAll = () => {
+  cardInner.forEach((e) => {
+    e.classList.add("flip");
+  });
+};
+
 //Using flex order to randomize board
 const shuffle = () => {
   cards.forEach((card) => {
@@ -85,5 +143,3 @@ const shuffle = () => {
     card.style.order = random;
   });
 };
-
-shuffle();
